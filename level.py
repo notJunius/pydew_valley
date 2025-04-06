9063ee94656a01fbc93d3d90e3804099907428a2
MJ Steel <morgan>
2025-04-06 09:24:48 -0600

    created a player file

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from settings import *
+from player import Player
 
 class Level:
     def __init__(self):
@@ -7,7 +8,10 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         #sprite groups
         self.all_sprites = pygame.sprite.Group()
+        self.setup()
     
+    def setup(self):
+        self.player = Player((640, 360), self.all_sprites)
 
     def run(self, dt):
         self.display_surface.fill('black')
